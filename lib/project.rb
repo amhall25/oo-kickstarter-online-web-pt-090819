6d07108e4b6ce337287e282e7ae5fc910e92d7ee
alycia hall <amhall25@gmail.com>
2019-10-27 20:17:46 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,7 @@ class Project
   
   def add_backer (backer)
     @backers<< backer
-    unless backer.back_project(self)
-    backer.back_project(self)
+    backer.backed_projects << self
   end
   end
 end
